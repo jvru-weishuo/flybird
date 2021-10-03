2a21fd0bead2c02d2d5327d5f2abf07164496b31
general <2711176731@qq.com>
2021-10-03 19:27:51 +0800

    biside

--- webs/server.js
+++ webs/server.js
@@ -3,4 +3,7 @@ const io = require('socket.io')(3000);
 io.on('connection',socket=>{
     socket.send('你好');
     console.log(socket.id);
+    socket.on('private message', ( anotherSocketId, msg ) => {
+        socket.to(anotherSocketId).emit('private message', socket.id, msg);
+    })
 })
